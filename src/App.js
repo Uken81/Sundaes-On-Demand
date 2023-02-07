8816369febeb329658dc36103fea2337424ba42e
Brendan Hurd <brendanhurd@gmail.com>
2023-02-07 13:38:23 +1100

    Test and code context for scoops subtotals.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import "./App.css";
+import { Container } from "react-bootstrap";
+import { OrderDetailsProvider } from "./contexts/OrderDetails";
+import OrderEntry from "./pages/entry/OrderEntry";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <OrderDetailsProvider>
+        <OrderEntry />
+      </OrderDetailsProvider>
+    </Container>
   );
 }
 
--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -1,6 +1,9 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Row from "react-bootstrap/Row";
+import { pricePerItem } from "../../constants";
+import { useOrderDetails } from "../../contexts/OrderDetails";
+import { formatCurrency } from "../../utillities";
 import AlertBanner from "../common/AlertBanner";
 import ScoopOptions from "./ScoopOption";
 import ToppingOptions from "./ToppingOption";
@@ -8,6 +11,7 @@ import ToppingOptions from "./ToppingOption";
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
   const [error, setError] = useState(false);
+  const { totals } = useOrderDetails();
 
   // optionType is 'scoops' or 'toppings
   useEffect(() => {
@@ -24,6 +28,7 @@ export default function Options({ optionType }) {
   }
 
   const ItemComponent = optionType === "scoops" ? ScoopOptions : ToppingOptions;
+  const title = optionType[0].toUpperCase() + optionType.slice(1).toLowerCase();
 
   const optionItems = items.map((item) => (
     <ItemComponent
@@ -33,5 +38,14 @@ export default function Options({ optionType }) {
     />
   ));
 
-  return <Row>{optionItems}</Row>;
+  return (
+    <>
+      <h2>{title}</h2>
+      <p>{formatCurrency(pricePerItem[optionType])} each</p>
+      <p>
+        {title} total: {formatCurrency(totals[optionType])}
+      </p>
+      <Row>{optionItems}</Row>
+    </>
+  );
 }
